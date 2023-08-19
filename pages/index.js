d4778be1d80fbcb564234e735eeb6fb447566802
Betsy Tsoi <tsoisybetsy@gmail.com>
2023-08-19 17:24:08 +0900

    Update homepage content

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Nora. Woof woof woof!</p>
+        <p>Hello, I'm Nora. Woof this is my mum's first nextjs app woof woof!</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
